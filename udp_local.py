66ab5421e22bcad9145756a43d8b88955efd1b28
Ying <yingzhu.yz@gmail.com>
2018-11-07 12:01:29 -0500

    udp_local.py finished

--- udp_local.py
+++ udp_local.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse, socket
+from datetime import datetime
+
+MAX_BYTES = 65535
 
 def client(port):
     print("I'm the client, send UDP packets to port %d of the server." % port)
+    cSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sentence = 'Current time is {}'.format(datetime.now())
+    msg = sentence.encode('ascii')
+    cSock.sendto(msg, ('127.0.0.1', port))
+    print('I was assigned the address {}'.format(cSock.getsockname()))
+    msg, address = cSock.recvfrom(MAX_BYTES)    # THINK TWICE!
+    sentence = msg.decode('ascii')
+    print('The server said: {}'.format(sentence))
 
 def server(port):
     print("I'm the server, listening at port %d." % port)
+    sSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sSock.bind(('127.0.0.1', port))
+    print('Listening at {}'.format(sSock.getsockname()))
 
+    while True:
+        msg, address = sSock.recvfrom(MAX_BYTES)
+        sentence = msg.decode('ascii')
+        print('Someone from {} said {!r}'.format(address, sentence))
+        sentence = 'I received from you a message of {} bytes'.format(len(msg))
+        msg = sentence.encode('ascii')
+        sSock.sendto(msg, address)
+    
 if __name__ == '__main__':
     roles = {'client': client, 'server': server}
     parser = argparse.ArgumentParser(description='send and receive UDP packets locally')
@@ -16,4 +38,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
     function = roles[args.role]
     function(args.port)
-
